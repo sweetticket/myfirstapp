d15ab5b4e232d0507de078b7e65726c37db072d7
jenny <yk494@cornell.edu>
2014-06-03 17:53:08 +0900

    start calc, two buttons, textbox

--- src/com/example/myfirstapp/MainActivity.java
+++ src/com/example/myfirstapp/MainActivity.java
@@ -14,8 +14,6 @@ import android.widget.EditText;
 import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
-
-	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,14 +64,4 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
-    /** Called when the user clicks the Send button */
-    public void sendMessage(View view){
-    	//Do something in response to button
-    	Intent intent = new Intent(this, DisplayMessageActivity.class);
-    	EditText editText = (EditText) findViewById(R.id.edit_message);
-    	String message = editText.getText().toString();
-    	intent.putExtra(EXTRA_MESSAGE, message);
-    	startActivity(intent);
-    }
-    
 }
