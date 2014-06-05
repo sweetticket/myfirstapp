505d0c953274937b64c9c5ecd2c0c7b9b387f9fb
jenny <yk494@cornell.edu>
2014-06-05 14:37:44 +0900

    calculate

--- src/com/example/myfirstapp/MainActivity.java
+++ src/com/example/myfirstapp/MainActivity.java
@@ -15,96 +15,127 @@ import android.widget.EditText;
 import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
-	
-	private String preinput = "";
 
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	private String mPreInput = "";
+	private double mLastInput = 0.0;
+	private String mOp = "";
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
-        public PlaceholderFragment() {
-        }
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
-    
-    /**
-     * Displays the digit clicked in text box. If digits are already displayed in the text box,
-     * add the digit to the end of the pre-displayed input.
-     */
-    public void displayDigit(View view) {
-    	if (preinput.length() < 10 ){
-    	Button b = (Button)view;
-    	String new_digit = b.getText().toString();
-    	String display_num = preinput + new_digit;
-    	preinput = display_num;
-    	EditText textbox = (EditText)findViewById(R.id.calc_input);
-    	textbox.setText(preinput);
-    	}
-    }
-    
-    /**
-     * Clears preinput.
-     */
-    public void clearInput(View view){
-    	preinput = "";
-    	EditText textbox = (EditText)findViewById(R.id.calc_input);
-    	textbox.setText(preinput);
-    	
-    }
-    
-    /**
-     * Changes sign of preinput
-     */
-    public void changeSigns(View view){
-    	if (preinput.length() > 0){
-    		if (preinput.substring(0, 1).equals("-")) {
-    			preinput = preinput.substring(1);
-    		}else {
-    			preinput = "-" + preinput;
-    			}
-    		EditText textbox = (EditText)findViewById(R.id.calc_input);
-    		textbox.setText(preinput);
-    	}
-    }
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			return rootView;
+		}
+	}
+
+	/**
+	 * Displays the digit clicked in text box. If digits are already displayed
+	 * in the text box, add the digit to the end of the pre-displayed input.
+	 */
+	public void displayDigit(View view) {
+		if (mPreInput.length() < 10) {
+			Button b = (Button) view;
+			String new_digit = b.getText().toString();
+			String display_num = mPreInput + new_digit;
+			mPreInput = display_num;
+			EditText textbox = (EditText) findViewById(R.id.calc_input);
+			textbox.setText(mPreInput);
+		}
+	}
+
+	/**
+	 * Clears preinput.
+	 */
+	public void clearInput(View view) {
+		mPreInput = "";
+		EditText textbox = (EditText) findViewById(R.id.calc_input);
+		textbox.setText(mPreInput);
+
+	}
+
+	/**
+	 * Changes sign of preinput
+	 */
+	public void changeSigns(View view) {
+		if (mPreInput.length() > 0) {
+			if (mPreInput.substring(0, 1).equals("-")) {
+				mPreInput = mPreInput.substring(1);
+			} else {
+				mPreInput = "-" + mPreInput;
+			}
+			EditText textbox = (EditText) findViewById(R.id.calc_input);
+			textbox.setText(mPreInput);
+		}
+	}
+
+	/**
+	 * Sets fields for add operation
+	 */
+	public void setOp(View view) {
+		Button b = (Button) view;
+		mOp = (String) b.getText();
+		if (mPreInput.length() > 0) {
+			mLastInput = Double.parseDouble(mPreInput);
+			mPreInput = "";
+			EditText textbox = (EditText) findViewById(R.id.calc_input);
+			textbox.setText(mPreInput);
+		}
+	}
+
+	/**
+	 * Calculates the final value using operation op, lastinput, and preinput.
+	 * Sets preinput to the final value. Displays final value in textbox.
+	 */
+	public void calculate(View view) {
+		EditText textbox = (EditText) findViewById(R.id.calc_input);
+		if (mOp.equals("+")) {
+			mPreInput = (mLastInput + Double.parseDouble(mPreInput)) + "";
+		} else if (mOp.equals("-")) {
+			mPreInput = (mLastInput - Double.parseDouble(mPreInput)) + "";
+		}
+		mOp = "";
+		mLastInput = 0.0;
+		textbox.setText(mPreInput);
+	}
+
 }
